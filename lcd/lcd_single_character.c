40655ba5df5d2790bd53df4eb663ab41bdc9f5f6
Shraven GS <gsshraven@gmail.com>
2025-11-10 17:53:26 +0530

    Create lcd_single_character.c

---
+++ lcd/lcd_single_character.c
@@ -0,0 +1,39 @@
+#include<REGX51.h>
+#include"delay.h"
+sbit RS=P3^0;
+sbit RW=P3^1;
+sbit E=P3^2;
+
+#define LCD P2
+
+void LCD_COMMAND(unsigned char cmd){
+	LCD=cmd;
+	RS=0;
+	RW=0;
+	E=1;
+	delay_ms(2);
+	E=0;
+}
+
+void LCD_DATA(unsigned char dat){
+	LCD=dat;
+	RS=1;
+	RW=0;
+	E=1;
+	delay_ms(2);
+	E=0;
+}
+
+void LCD_INIT(){
+	LCD_COMMAND(0x38);
+	LCD_COMMAND(0x0C);
+	LCD_COMMAND(0x06);
+	LCD_COMMAND(0x01);
+	LCD_COMMAND(0x80);
+}
+
+int main(){
+LCD_INIT();
+	LCD_DATA('A');
+	while(1);
+}
